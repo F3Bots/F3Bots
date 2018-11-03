dc451f2348f7d6ae67fb1dca6e84f1404c0562dc
F3Bots <44725715+F3Bots@users.noreply.github.com>
2018-11-03 11:58:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -652,7 +652,6 @@ client.on('message', message => {
     『+mutechannel / لقفل الشات  』
     『+unmutechannel / لفتح الشات 』
     『+bc / لارسال رسالة لجميع اعضاء السيرفر 』
-    『+kick / لطرد شخص من السيرفر 』
     『+mute / لاعطاء شخص ميوت 』
     『+unmute / لفك ميوت شخص 』
     『+giveaway / لعمل قيف اواي 』
@@ -714,44 +713,5 @@ client.on('message', message => {
     });
     
     
-
-
-
-
-
-client.on('message', message => {
-      if (message.author.kick) return;
-       if(message.content.startsWith("+kick")) {return;
-     
-      let command = message.content.split(" ")[0];
-      command = command.slice(prefix.length);
-     
-      let args = message.content.split(" ").slice(1);
-     
-      if (command == "kick") {
-                   if(!message.channel.guild) return;
-             
-      if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
-      if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
-      let user = message.mentions.users.first();
-      let reason = message.content.split(" ").slice(2).join(" ");
-     
-      if (message.mentions.users.size < 1) return message.reply("منشن شخص");
-      if(!reason) return message.reply ("اكتب سبب الطرد");
-      if (!message.guild.member(user)
-      .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
-     
-      message.guild.member(user).kick(7, user);
-     
-      const banembed = new Discord.RichEmbed()
-      .setAuthor('Kicked !', user.displayAvatarURL)
-      .setColor("RANDOM")
-      .setTimestamp()
-      .addField("User:",  `[ + ${user.tag} + ]`)
-      .addField("By:", `[  + ${message.author.tag} +  ]`)
-      .addField("Reason:", `[ + ${reason} +  ]`)
-      client.channels.get("492583022982463500").send({embed : banembed})
-    }
-    });
 
 client.login(process.env.BOT_TOKEN);
